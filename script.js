488d6f7c9d05e57776fff5f70411d1483ff66c45
Andrew Gunkel <65040985+andrewgunkel@users.noreply.github.com>
2026-02-09 12:58:08 +0100

    Added initial board functionality and simple player marking

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
 function Gameboard() {
-    const rows = 3;
-    const cols = 3;
 
   const board = Array(9).fill(null);
 
-    return { board };
+    //return { board };
+    function placeSymbol(position, symbol) {
+        if (board[position] === null) {
+            board[position] = symbol;
+        }
+    }
 
-}
 
-const gameboard = Gameboard();
-
-console.log(gameboard.board);
-
-function printBoard(board) {
+    //draw board in console
+    function printBoard(board) {
+    //if the cell is null, display the index, otherwise display the symbol
     const display = board.map((cell, index) =>
         cell === null ? index : cell
     );
 
     console.log(`
-${display[0]} | ${display[1]} | ${display[2]}
----------
-${display[3]} | ${display[4]} | ${display[5]}
----------
-${display[6]} | ${display[7]} | ${display[8]}
-`);
+        ${display[0]} | ${display[1]} | ${display[2]}
+        ---------
+        ${display[3]} | ${display[4]} | ${display[5]}
+        ---------
+        ${display[6]} | ${display[7]} | ${display[8]}
+        `);
+    }
+
+    return { board, placeSymbol, printBoard };
 }
 
+
+
+
+const gameboard = Gameboard();
+
+gameboard.printBoard(gameboard.board);
+
+//place symbol - gameboard.placeSymbol(0, 'X');
+//print board - gameboard.printBoard(gameboard.board);
 
 function Player(name, symbol) {
     return { name, symbol };
